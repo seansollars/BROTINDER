a2108fd2580395aa9d421a2992f184a9730046d5
Sean Sollars <seansollars@amdg.rockhursths.edu>
2017-04-18 11:24:09 -0500

    Server setup part three

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-var express = require('express') 
-var app = express()
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.get('/', function (req,res) {
 	res.send('Hello Bro')
+});
+
+app.post('/', function(req,res) {
+	console.log('post / =' + JSON.stringify(req.body));
+	res.status(200).send('got it');
 });
 
 app.listen(3000,function () {
