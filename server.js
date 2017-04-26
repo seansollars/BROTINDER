3115f399e2c69da59bb502537e03f12353309802
Sean Sollars <seansollars@amdg.rockhursths.edu>
2017-04-26 12:10:50 -0500

    CLIENT PART ONE

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 var http = require('http');
 var socketio = require('socket.io');
-
+var Mongo = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 var express = require('express');
+
+const MONGO_URL = 'mongodb://localhost:27017/iot';
+
+Mongo.connect(MONGO_URL, function(err,db) {
+	if(err) {
+		// TODO: handle error
+	}
+
+	Mongo.ops = {};
+	
+	
+	Mongo.ops.find = function(collection,json,callback) {
+	db.collection(collection).find(json).toArray(function(err,docs) {
+		if(callback) callback(err,docs);
+	});
+};
+	
+	Mongo.ops.insert = function(collection,json, callback) {
+		db.collection(collection).insert(json,function(err,docs) {
+			if(callback) callback(err,result);
+		});
+	};
+});
 
 var server = http.createServer(app);
 var io = socketio(server);
@@ -11,11 +34,12 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/',express.static('../WebClient/'));
+
+app.use('/', express.static('../WebClient/'));
 
 
-app.get('/', function (req,res) {
-	res.send('Hello Bro')
-});
+
 
 app.post('/', function(req,res) {
 	console.log('post / =' + JSON.stringify(req.body));
@@ -24,9 +48,15 @@ app.post('/', function(req,res) {
 
 app.post('/echo', function(req,res) {
 	console.log('post /echo = ' + JSON.stringify(req.body));
-	io.socketd.emit('echo', req.body);
-	res.status(201).send(req.body);
-}
+	io.sockets.emit('echo', req.body);
+	Mongo.ops.insert('echo', req.body,function(err,result) {
+		if(err)
+			res.status(500).send(error);
+	else 
+		res.status(201).send(req.body);
+   });
+});
+
 
 server.listen(3000,function () {
 	console.log('example app listening on port 3000!')
